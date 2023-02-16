d5436ecd9ac76c3f1bbe0b324cb4c3b67bee3669
bourgeoa <alain.bourgeois10@gmail.com>
2023-02-16 19:00:00 +0100

    TextEncoder/TextDecoder

--- jest.setup.js
+++ jest.setup.js
@@ -1,4 +1,8 @@
 // import "@testing-library/jest-dom";
 const fetchMock = require('jest-fetch-mock')
+const { TextEncoder, TextDecoder } = require('util')
+
+global.TextEncoder = TextEncoder;
+global.TextDecoder = TextDecoder
 
 fetchMock.enableMocks();
