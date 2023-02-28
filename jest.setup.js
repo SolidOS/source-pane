24b9a8ac465503e25d70f43fd5b26316ab2c8408
Alain Bourgeois <alain.bourgeois10@gmail.com>
2023-02-28 19:47:19 +0100

    Update jest.setup.js

--- jest.setup.js
+++ jest.setup.js
@@ -1,9 +1,4 @@
 // import "@testing-library/jest-dom";
-const { TextEncoder, TextDecoder } = require('util')
-
-global.TextEncoder = TextEncoder;
-global.TextDecoder = TextDecoder
-
 const fetchMock = require('jest-fetch-mock')
 const { TextEncoder, TextDecoder } = require('util')
 
