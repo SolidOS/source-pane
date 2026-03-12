e7052444d7c9c0bcd661e7aa6dec95b1ac87480e
Sharon Stratsianis <sstratsianis@gmail.com>
2026-03-13 10:44:59 +1100

    add module rules for consolidation

--- webpack.config.mjs
+++ webpack.config.mjs
@@ -1,4 +1,5 @@
 import path from 'path'
+import { moduleRules } from './webpack.module.rules.mjs'
 import { createRequire } from 'module'
 import TerserPlugin from 'terser-webpack-plugin'
 import CopyPlugin from 'copy-webpack-plugin'
@@ -6,57 +7,34 @@ import CopyPlugin from 'copy-webpack-plugin'
 const require = createRequire(import.meta.url)
 
 const common = {
-  entry: './src/index.ts',
-  performance: {
-    hints: false
-  },
-  resolve: {
-    extensions: ['.ts', '.js', '.json'],
-    fallback: {
-      path: require.resolve('path-browserify')
-    }
-  },
+  mode: 'production',
+  entry: './src/index.js',
   module: {
-    rules: [
-      {
-        test: /\.ts$/,
-        use: 'ts-loader',
-        exclude: /node_modules/,
-      },
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
-      },
-      {
-        test: /\.ttl$/i,
-        type: 'asset/source'
-      }
-    ],
+    rules: moduleRules,
   },
   externals: {
-    'fs': 'null',
-    'node-fetch': 'fetch',
-    'isomorphic-fetch': 'fetch',
-    'text-encoding': 'TextEncoder',
-    '@trust/webcrypto': 'crypto',
-    'rdflib': 'rdflib',
+    'solid-ui': 'UI',
     'solid-logic': 'SolidLogic',
-    'solid-ui': 'UI'
+    rdflib: '$rdf',
+  },
+  resolve: {
+    extensions: ['.js', '.ts'],
+    fallback: {
+      path: require.resolve('path-browserify')
+    },
   },
   devtool: 'source-map',
 }
-
-
 
 const normalConfig = {
   ...common,
   mode: 'production',
   output: {
     path: path.resolve(process.cwd(), 'lib'),
-    filename: 'profile-pane.js',
+    filename: 'issue-pane.js',
     library: {
       type: 'umd',
-      name: 'ProfilePane',
+      name: 'IssuePane',
       export: 'default',
     },
     globalObject: 'this',
@@ -83,10 +61,10 @@ const minConfig = {
   mode: 'production',
   output: {
     path: path.resolve(process.cwd(), 'lib'),
-    filename: 'profile-pane.min.js',
+    filename: 'issue-pane.min.js',
     library: {
       type: 'umd',
-      name: 'ProfilePane',
+      name: 'IssuePane',
       export: 'default',
     },
     globalObject: 'this',
